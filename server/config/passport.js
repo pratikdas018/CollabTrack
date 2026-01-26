353baea56d5bdd14ad246dee4180c3964ba9a622
Pratik Chandra Das <pratikdassonu@gmail.com>
2026-01-26 12:54:43 +0530

    Update GitHub callback URL to use environment variable

--- server/config/passport.js
+++ server/config/passport.js
@@ -17,7 +17,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "https://collabtrack-cbg7.onrender.com/api/auth/github/callback"
+      callbackURL: `${process.env.SERVER_URL || 'http://localhost:5000'}/api/auth/github/callback`
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
